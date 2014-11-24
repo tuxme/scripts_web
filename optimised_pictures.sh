24ccd051017de64fd3073302cfa5d250f21c8429
tuxme <berzerking@ks3283877.kimsufi.com>
2014-11-24 15:55:18 +0100

    ajout extension

--- optimised_pictures.sh
+++ optimised_pictures.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 DATE=`date +%Y%m%d`
+extenstion=$1
+if [[ -z $extenstion ]]
+	then
+		echo "Vous devez indiquer une extension d'images : ex png/jpg/jpeg ..."
+		exit
+fi
 BIN_VERIF=`which pngnq`
 if [[ -z $BIN_VERIF ]]
 	then
@@ -8,7 +14,7 @@ if [[ -z $BIN_VERIF ]]
 		echo "redhat/fedora : sudo yum install pngnq"
 fi
 
-for var in `find . | grep "\.png$"`
+for var in `find . | grep "\.$extenstion$"`
 	do
 		echo -n "Traitement de $var : "
 		TEST=`file $var| grep -v image`
@@ -16,9 +22,9 @@ for var in `find . | grep "\.png$"`
 			then
 				DATA_ORIG=`ls -s1 $var | cut -d" " -f1`
 				cp $var ${var}_orig_${DATE}
-				BASE_FILE=`echo $var|sed "s#\.png\\$##g"`
-				$BIN_VERIF -s 1 -e '.newpng' -f $var
-				mv ${BASE_FILE}.newpng $var
+				BASE_FILE=`echo $var|sed "s#\.$extenstion\\$##g"`
+				$BIN_VERIF -s 1 -e ".new${extenstion}" -f $var
+				mv ${BASE_FILE}.new${extenstion} $var
 				DATA_NEW=`ls -s1 $var | cut -d" " -f1`
 				TOT=`echo $((DATA_ORIG - DATA_NEW))`
 				echo -e "$TOT\n"
